ca26851e31d950a0ab20c68ea93409756e4adddb
Carlos <84628913+MaGaiDeN@users.noreply.github.com>
2024-11-15 17:03:49 +0100

    Limpieza de node_modules y actualización de .gitignore

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const { authenticateUser } = require('../middlewares/auth');
-
-router.get('/profile', authenticateUser, userController.getProfile);
-router.put('/profile', authenticateUser, userController.updateProfile);
-
-module.exports = router; 
